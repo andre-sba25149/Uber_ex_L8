67f66f0999dc970e217d4276f4e9f946bf524c0f
andre-sba25149 <sba25149@student.cct.ie>
2026-05-12 19:02:31 +0100

    Update uber.py

--- uber.py
+++ uber.py
@@ -19,6 +19,14 @@ def load_data(nrows):
 	data[DATE_COLUMN]= pd.to_datetime(data[DATE_COLUMN])
 	return data
 
-data_load_state=st.text("Loanding Data...")
+data_load_state=st.text("Loading Data...")
 data=load_data(10000)
-data_load_state.text("Loanding Data done!!")
+data_load_state.text("Loading Data done!!")
+
+
+st.subheader("Raw Data")
+st.write(data)
+
+st.subheader("Num Pickups per hour")
+hist_values=np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
